eb6e65cdb2da7c895dcf51feafe09be4cb00335b
bgibso <42751984+bgibso@users.noreply.github.com>
2018-11-12 22:19:25 -0500

    m

---
+++ css/css/jquery.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	demoFunction();
+
+	$(window).resize(function(){
+
+	});
+
+	$(window).scroll(function() {
+
+	});
+});
+
+function demoFunction() {
+	$('p').click(function(){
+		$('p').toggleClass('bigger');
+		$('body').addClass('bgColor');
+	});
+}
