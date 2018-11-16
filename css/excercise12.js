40f66d286de00c9e38e6b23653aa9f4ed9dd28db
bgibso <42751984+bgibso@users.noreply.github.com>
2018-11-15 22:40:46 -0500

    j

--- css/excercise12.js
+++ css/excercise12.js
@@ -8,16 +8,41 @@ $(document).ready(function() {
    
     });
  
-    
+
     $(window).scroll(function() {
  
     });
 });
 
 function interactions() {
-    $('.jqClick').click(function(){
+    $('.blue').click(function(){
         $('.ow').toggleClass('moveITL');
         console.log('clicked');
     });
+    $('.ow').click(function(){
+        $('.batman').toggleClass('moveITL');
+        console.log('clicked');
+    });
+    $('.batman').click(function(){
+        $('.skyrim').toggleClass('moveITL');
+        console.log('clicked');
+    });
+    $('.skyrim').click(function(){
+        $('.bros').toggleClass('moveITL');
+        console.log('clicked');
+    });
+    $('.bros').click(function(){
+        $('.Halo').toggleClass('moveITL');
+        console.log('clicked');
+    });
+    $('.Halo').click(function(){
+        $('.blue').toggleClass('moveITL');
+        console.log('clicked');
+    });
     
+ 
+function variables() {
+    var h1H = $('h1').height();
+    var wH = $(window).height();
+    console.log(h1H);
 }
